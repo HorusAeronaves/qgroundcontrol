a929d2f8eb758020f214425fb4f1996c1b476eb2
Patrick José Pereira <patrickelectric@gmail.com>
2017-10-24 17:06:52 -0200

    Change pallet and icons
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/QGCPalette.cc
+++ src/QGCPalette.cc
@@ -78,8 +78,8 @@ void QGCPalette::_buildMap(void)
     // Colors are not affecting by theming
     DECLARE_QGC_COLOR(mapWidgetBorderLight, "#ffffff", "#ffffff", "#ffffff", "#ffffff")
     DECLARE_QGC_COLOR(mapWidgetBorderDark,  "#000000", "#000000", "#000000", "#000000")
-    DECLARE_QGC_COLOR(brandingPurple,       "#4A2C6D", "#4A2C6D", "#4A2C6D", "#4A2C6D")
-    DECLARE_QGC_COLOR(brandingBlue,         "#48D6FF", "#48D6FF", "#48D6FF", "#48D6FF")
+    DECLARE_QGC_COLOR(brandingPurple,       "#005378", "#005378", "#005378", "#005378")
+    DECLARE_QGC_COLOR(brandingBlue,         "#009dc1", "#009dc1", "#009dc1", "#009dc1")
 }
 
 void QGCPalette::setColorGroupEnabled(bool enabled)
